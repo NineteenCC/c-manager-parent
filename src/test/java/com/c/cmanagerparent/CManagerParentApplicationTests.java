bd279b84868f583d08cea953dc339cf6508aeb4a
NineteenCC <95668230+NineteenCC@users.noreply.github.com>
2022-12-06 17:25:40 +0800

    Initial commit

---
+++ src/test/java/com/c/cmanagerparent/CManagerParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.cmanagerparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CManagerParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
